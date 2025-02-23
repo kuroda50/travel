c7c6a24b6f220331737889bf7cbc814e4f931e86
kuroda50 <144975768+kuroda50@users.noreply.github.com>
2025-02-23 11:20:55 +0900

    ログインを作った（嘘） #9

--- lib/component/bottom_navigation_bar.dart
+++ lib/component/bottom_navigation_bar.dart
@@ -10,6 +10,7 @@ class CustomBottomNavigationBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // aaa
       body: child,
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _getSelectedIndex(context),

8116a6f3551dff843aef918f86bc8db568cf3dcd
Keitayas2 <keita.y2004@icloud.com>
2025-02-25 12:09:57 +0900

    設定画面を作成

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -9,8 +9,49 @@ class SettingsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('設定'),
       ),
-      body: const Center(
-        child: Text('設定画面'),
+      body: Center(
+        child: Column(
+          children: [
+            buildButton('メールアドレスを変更する', isFirst: true),
+            buildButton('パスワードを変更する'),
+            buildButton('ログアウト'),
+            buildButton('アカウントを削除する', isLast: true),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildButton(String text, {bool isFirst = false, bool isLast = false}) {
+    BorderRadius borderRadius = BorderRadius.only(
+      topLeft: isFirst ? const Radius.circular(16) : Radius.zero,
+      topRight: isFirst ? const Radius.circular(16) : Radius.zero,
+      bottomLeft: isLast ? const Radius.circular(16) : Radius.zero,
+      bottomRight: isLast ? const Radius.circular(16) : Radius.zero,
+    );
+
+    return Container(
+      width: 300,
+      height: 50,
+      decoration: BoxDecoration(
+        borderRadius: borderRadius,
+        border: Border.all(color: Colors.black),
+      ),
+      child: SizedBox(
+        height: 50,
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
+            shape: RoundedRectangleBorder(
+              borderRadius: borderRadius,
+              side: const BorderSide(color: Colors.black),
+            ),
+            padding: EdgeInsets.zero,
+          ),
+          child: Text(text),
+        ),
       ),
     );
   }

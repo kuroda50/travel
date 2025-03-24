34376282f9d4ec5ca1f7abd2ac45015333d3c7be
kuroda50 <144975768+kuroda50@users.noreply.github.com>
2025-03-24 17:05:59 +0900

    メール変更用のファイルを追加した

---
+++ lib/screens/email_change_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EmailChangeScreen extends StatelessWidget {
+  const EmailChangeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/screens/email_change_screen2.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EmailChangeScreen2 extends StatelessWidget {
+  const EmailChangeScreen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

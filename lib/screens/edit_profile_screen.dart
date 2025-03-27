8e624d4a3bcbf8c88d76d40bf190f5bb6f25f24e
kuroda50 <144975768+kuroda50@users.noreply.github.com>
2025-03-27 10:02:39 +0900

    プロフィール編集画面を修正 #139

--- lib/screens/edit_profile_screen.dart
+++ lib/screens/edit_profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart'; // go_routerをインポート
 import 'package:travel/component/header.dart'; // Headerウィジェットをインポート
@@ -14,27 +15,61 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
   static const Color mainButtonColor = Color(0xFF559900); // 保存ボタンのカラー
 
   final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _hobbiesController = TextEditingController();
   final TextEditingController _bioController = TextEditingController();
+  final TextEditingController hobbyController = TextEditingController();
   bool _isMale = true;
   DateTime _selectedDate = DateTime.now();
+  bool _isLoading = true; // ローディング状態を管理
+  List<String> hobbies = [];
+  String? _genderError;
 
   // Firebase Firestoreインスタンスを取得
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  void _saveProfile() async {
+  @override
+  void initState() {
+    super.initState();
+    _loadUserProfile();
+  }
+
+  Future<void> _loadUserProfile() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) return;
+
+    final docRef = FirebaseFirestore.instance.collection('users').doc(user.uid);
+    final docSnap = await docRef.get();
+
+    if (docSnap.exists) {
+      final data = docSnap.data()!;
+      setState(() {
+        _nameController.text = data['name'] ?? '';
+        hobbies = (data['hobbies'] as List<dynamic>).cast<String>();
+        _bioController.text = data['bio'] ?? '';
+        _isMale = data['gender'] == 'male';
+        _selectedDate = (data['birthday'] != null)
+            ? (data['birthday'] as Timestamp).toDate()
+            : DateTime.now();
+        _isLoading = false;
+      });
+    }
+  }
+
+  Future<void> _saveProfile() async {
     final name = _nameController.text;
-    final hobbies = _hobbiesController.text;
     final bio = _bioController.text;
 
+    String? userId = FirebaseAuth.instance.currentUser?.uid;
+    if (userId == null) return;
+    final userRef = await _firestore.collection("users").doc(userId);
     try {
       // Firestoreに保存する
-      await _firestore.collection('users').add({
+      await userRef.update({
         'name': name,
-        'gender': _isMale ? '男性' : '女性',
-        'birthdate': _selectedDate,
+        'gender': _isMale ? 'male' : 'female',
+        'birthday': _selectedDate,
         'hobbies': hobbies,
         'bio': bio,
+        'updatedAt': FieldValue.serverTimestamp()
       });
 
       // 保存成功のメッセージ
@@ -43,7 +78,7 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
       );
 
       // /profileに遷移
-      context.go('/profile', extra: 'ユーザーID'); // ユーザーIDは適切なものに置き換えてください
+      context.pop('/profile');
     } catch (e) {
       // 保存失敗のエラーメッセージ
       ScaffoldMessenger.of(context).showSnackBar(
@@ -68,8 +103,76 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
     }
   }
 
+  Widget _buildGenderSelection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          '性',
+          style: TextStyle(fontSize: 13, color: Colors.black87),
+        ),
+        SizedBox(height: 4),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: ToggleButtons(
+            isSelected: [_isMale, !_isMale],
+            onPressed: (int index) {
+              setState(() {
+                _isMale = !_isMale;
+                _genderError = null;
+              });
+            },
+            borderRadius: BorderRadius.circular(7),
+            selectedColor: Colors.white, // 選択時の文字色
+            fillColor: Colors.green, // 選択時の背景色
+            borderColor: Colors.black87, // 枠線の色
+            borderWidth: 0.6, // 枠線の太さ
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 12),
+                child: Row(
+                  children: [
+                    Icon(Icons.female, color: Colors.red),
+                    SizedBox(width: 4),
+                    Text('女性',
+                        style: TextStyle(color: Colors.black87)), // Textの色も変更
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 12),
+                child: Row(
+                  children: [
+                    Icon(Icons.male, color: Colors.blue),
+                    SizedBox(width: 4),
+                    Text('男性',
+                        style: TextStyle(color: Colors.black87)), // Textの色も変更
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        if (_genderError != null) // エラーメッセージを表示
+          Padding(
+            padding: EdgeInsets.only(top: 5),
+            child: Text(
+              _genderError!,
+              style: TextStyle(color: Colors.red, fontSize: 12),
+            ),
+          ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (_isLoading) {
+      return Scaffold(
+        appBar: Header(title: "プロフィール編集"),
+        body: Center(child: CircularProgressIndicator()),
+      );
+    }
     return Scaffold(
       appBar: Header(title: "プロフィール編集"), // ヘッダーを追加
       body: Padding(
@@ -88,33 +191,7 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
             SizedBox(height: 16),
             Text('性別:', style: TextStyle(fontSize: 18)),
             SizedBox(height: 8),
-            Row(
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      _isMale = true;
-                    });
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: _isMale ? Colors.blue : Colors.grey,
-                  ),
-                  child: Text('男性'),
-                ),
-                SizedBox(width: 16),
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      _isMale = false;
-                    });
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: !_isMale ? Colors.pink : Colors.grey,
-                  ),
-                  child: Text('女性'),
-                ),
-              ],
-            ),
+            _buildGenderSelection(),
             SizedBox(height: 16),
             Text('誕生日:', style: TextStyle(fontSize: 18)),
             SizedBox(height: 8),
@@ -135,12 +212,50 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
             SizedBox(height: 16),
             Text('趣味:', style: TextStyle(fontSize: 18)),
             SizedBox(height: 8),
-            TextField(
-              controller: _hobbiesController,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: '趣味を入力',
-              ),
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: hobbyController,
+                    decoration: InputDecoration(
+                      hintText: '趣味を入力',
+                    ),
+                    onSubmitted: (value) {
+                      if (value.isNotEmpty) {
+                        setState(() {
+                          hobbies.add(value);
+                          hobbyController.clear();
+                        });
+                      }
+                    },
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () {
+                    if (hobbyController.text.isNotEmpty) {
+                      setState(() {
+                        hobbies.add(hobbyController.text);
+                        hobbyController.clear();
+                      });
+                    }
+                  },
+                ),
+              ],
+            ),
+            Wrap(
+              spacing: 8.0,
+              children: hobbies
+                  .map((hobby) => Chip(
+                        label: Text(hobby),
+                        deleteIcon: Icon(Icons.cancel), // バツマークのアイコン
+                        onDeleted: () {
+                          setState(() {
+                            hobbies.remove(hobby); // タップされたタグをリストから削除
+                          });
+                        },
+                      ))
+                  .toList(),
             ),
             SizedBox(height: 16),
             Text('自己紹介:', style: TextStyle(fontSize: 18)),
